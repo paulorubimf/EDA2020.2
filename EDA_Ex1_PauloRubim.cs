1d51d6bd2805f070f42697666d7793db3bf56b61
PauloRubim <paulo789rubim@gmail.com>
2021-03-16 22:22:55 -0300

    first commit

---
+++ EDA_Ex1_PauloRubim.cs
@@ -0,0 +1,219 @@
+using System;
+using System.Diagnostics;
+
+namespace Atividade1
+{
+	public class EDA1
+	{
+		public static void Main()
+		{
+			int[] vetor1 = new int[1000];
+			int[] vetor2 = new int[5000];
+			Random x = new Random();
+			Stopwatch stopwatch = new Stopwatch();
+			var r = System.Diagnostics.Stopwatch.StartNew();
+			// -----1000
+			stopwatch.Start();
+			for (int i = 0; i < vetor1.Length; i++)
+			{
+				vetor1[i] = x.Next(1, 1000);
+			}
+
+			bubbleSort(vetor1, vetor1.Length);
+			Console.WriteLine("\n\nVetor Ordenado pelo bubbleSort para 1000 valores\n");
+			for (int i = 0; i < vetor1.Length; i++)
+			{
+				Console.Write("{0} ", vetor1[i]);
+			}
+
+			r.Stop();
+			Console.WriteLine("\n\nTempo Decorrido do bubbleSort: {0}", r.Elapsed);
+			if (!r.IsRunning)
+				r.Restart();
+			insertionSort(vetor1, vetor1.Length);
+			Console.WriteLine("\n\nVetor Ordenado pelo insertionSort para 1000 valores\n");
+			for (int i = 0; i < vetor1.Length; i++)
+			{
+				Console.Write("{0} ", vetor1[i]);
+			}
+
+			r.Stop();
+			Console.WriteLine("\n\nTempo Decorrido do insertionSort: {0}", r.Elapsed);
+			if (!r.IsRunning)
+				r.Restart();
+			selectionSort(vetor1, vetor1.Length);
+			Console.WriteLine("\n\nVetor Ordenado pelo selectionSort para 1000 valores\n");
+			for (int i = 0; i < vetor1.Length; i++)
+			{
+				Console.Write("{0} ", vetor1[i]);
+			}
+
+			r.Stop();
+			Console.WriteLine("\n\nTempo Decorrido do selectionSort: {0}", r.Elapsed);
+			if (!r.IsRunning)
+				r.Restart();
+			quickSort(vetor1);
+			Console.WriteLine("\n\nVetor Ordenado pelo quickSort para 1000 valores\n");
+			for (int i = 0; i < vetor1.Length; i++)
+			{
+				Console.Write("{0} ", vetor1[i]);
+			}
+
+			r.Stop();
+			Console.WriteLine("\n\nTempo Decorrido do quickSort: {0}", r.Elapsed);
+			if (!r.IsRunning)
+				r.Restart();
+			//5000
+			for (int i = 0; i < vetor2.Length; i++)
+			{
+				vetor2[i] = x.Next(1, 5000);
+			}
+
+			bubbleSort(vetor2, vetor2.Length);
+			Console.WriteLine("\n\nVetor Ordenado pelo bubbleSort para 5000 valores\n");
+			for (int i = 0; i < vetor2.Length; i++)
+			{
+				Console.Write("{0} ", vetor2[i]);
+			}
+
+			r.Stop();
+			Console.WriteLine("\n\nTempo Decorrido do bubbleSort: {0}", r.Elapsed);
+			if (!r.IsRunning)
+				r.Restart();
+			insertionSort(vetor2, vetor2.Length);
+			Console.WriteLine("\n\nVetor Ordenado pelo insertionSort para 5000 valores\n");
+			for (int i = 0; i < vetor2.Length; i++)
+			{
+				Console.Write("{0} ", vetor2[i]);
+			}
+
+			r.Stop();
+			Console.WriteLine("\n\nTempo Decorrido do insertionSort: {0}", r.Elapsed);
+			if (!r.IsRunning)
+				r.Restart();
+			selectionSort(vetor2, vetor2.Length);
+			Console.WriteLine("\n\nVetor Ordenado pelo selectionSort para 5000 valores\n");
+			for (int i = 0; i < vetor2.Length; i++)
+			{
+				Console.Write("{0} ", vetor2[i]);
+			}
+
+			r.Stop();
+			Console.WriteLine("\n\nTempo Decorrido do selectionSort: {0}", r.Elapsed);
+			if (!r.IsRunning)
+				r.Restart();
+			quickSort(vetor2);
+			Console.WriteLine("\n\nVetor Ordenado pelo quickSort para 5000 valores\n");
+			for (int i = 0; i < vetor2.Length; i++)
+			{
+				Console.Write("{0} ", vetor2[i]);
+			}
+
+			r.Stop();
+			Console.WriteLine("\n\nTempo Decorrido do quickSort: {0}", r.Elapsed);
+			if (!r.IsRunning)
+				r.Restart();
+			stopwatch.Stop();
+			Console.WriteLine("\n\nO Tempo total Decorrido: {0}", stopwatch.Elapsed);
+		}
+
+		static void bubbleSort(int[] vetor, int length)
+		{
+			int trocas = 0;
+			for (int i = 0; i < length - 1; i++)
+			{
+				for (int j = 0; j < length - 1; j++)
+				{
+					if (vetor[j] > vetor[j + 1])
+					{
+						trocas = vetor[j];
+						vetor[j] = vetor[j + 1];
+						vetor[j + 1] = trocas;
+					}
+				}
+			}
+		}
+
+		static void insertionSort(int[] vetor, int length)
+		{
+			int j, atual = 0;
+			for (int i = 0; i < length; i++)
+			{
+				atual = vetor[i];
+				j = i - 1;
+				while (j >= 0 && vetor[j] > atual)
+				{
+					vetor[j + 1] = vetor[j];
+					j = j - 1;
+				}
+
+				vetor[j + 1] = atual;
+			}
+		}
+
+		static int[] selectionSort(int[] vetor, int length)
+		{
+			int min, aux;
+			for (int i = 0; i < length - 1; i++)
+			{
+				min = i;
+				for (int j = i + 1; j < length; j++)
+				{
+					if (vetor[j] < vetor[min])
+						min = j;
+				}
+
+				if (min != i)
+				{
+					aux = vetor[min];
+					vetor[min] = vetor[i];
+					vetor[i] = aux;
+				}
+			}
+
+			return vetor;
+		}
+
+		public static int[] quickSort(int[] vetor)
+		{
+			int inicio = 0;
+			int fim = vetor.Length - 1;
+			quickSort(vetor, inicio, fim);
+			return vetor;
+		}
+
+		private static void quickSort(int[] vetor, int inicio, int fim)
+		{
+			if (inicio < fim)
+			{
+				int p = vetor[inicio];
+				int i = inicio + 1;
+				int f = fim;
+				while (i <= f)
+				{
+					if (vetor[i] <= p)
+					{
+						i++;
+					}
+					else if (p < vetor[f])
+					{
+						f--;
+					}
+					else
+					{
+						int troca = vetor[i];
+						vetor[i] = vetor[f];
+						vetor[f] = troca;
+						i++;
+						f--;
+					}
+				}
+
+				vetor[inicio] = vetor[f];
+				vetor[f] = p;
+				quickSort(vetor, inicio, f - 1);
+				quickSort(vetor, f + 1, fim);
+			}
+		}
+	}
+}
